3db1319ed0e931d4d9e1545457623db5e65ebc61
Lautaro Bernardini <lauerber@hotmail.com.ar>
2024-03-26 22:00:28 -0300

    parte a

---
+++ codigo.js
@@ -0,0 +1,20 @@
+// Crear una función que reciba como parámetro una nota y retorne una de 
+// estas 3 palabras: LIBRE, REGULAR o PROMOCIONADO, en dependencia de:
+
+// Nota es menor a 4 - LIBRE
+// Nota es menor a 7 y no es LIBRE - REGULAR 
+// Nota mayor que 6  - PROMOCIONADO.
+
+function evaluar(nota) {
+
+    if (nota < 4) {
+        return "LIBRE"
+    } else if (nota < 7) {
+        return "REGULAR"
+    } else if (nota > 6) {
+        return "PROMOCIONADO"
+    }
+
+}
+
+console.log(evaluar(10))
